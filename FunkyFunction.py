365b3b4202255cb3035d1d3b93a914b7cc02a46b
ajstone172 <ajstone172@gmail.com>
2025-01-26 10:47:28 -0500

    Create FunkyFunction.py
    #Louis Armstrong's song "Do You Know What It Means to Miss New Orleans" is a favorite song of mine. I wrote a simple function to substitute the name of any other city.

---
+++ FunkyFunction.py
@@ -0,0 +1,15 @@
+#fun with a funky function!
+#Louis Armstrong's song "Do You Know What It Means to Miss New Orleans"
+#is a favorite song of mine
+#I wrote a simple function to substitute the name of any other city
+
+def funky(city):
+    print(f"Do you know what it means to miss {city}")
+    print("And miss it each night and day?")
+    print("I know I’m not wrong")
+    print("the feeling's getting stronger")
+    print("The longer I stay away")
+    print( )
+
+input = input("Name a city: ")
+print(funky(input))
